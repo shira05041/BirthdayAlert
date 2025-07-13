309e2a9c313a938404c423eb127b8ebef73f0f5f
Meir Weissman <shira05041@gmail.com>
2025-07-13 13:35:36 +0300

    adding main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from website import create_app, create_database
+import os
+
+app = create_app()
+create_database(app)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
